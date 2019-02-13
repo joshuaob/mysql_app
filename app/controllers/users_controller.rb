8d86bdaec9c1967733d7d13b56d7a2b94beae277
Joshua Ob <joshuaaob@gmail.com>
2019-02-13 22:35:28 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :last_sign_in_ip)
+      params.require(:user).permit(:name, :email)
     end
 end
